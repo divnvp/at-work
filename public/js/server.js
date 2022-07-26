af17c90c42287c2f39d20ec4b12ff37264ca9ec9
Diana <divnvp@yandex.com>
2022-07-26 11:17:42 +0900

    add: server file

---
+++ public/js/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // read .env files
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(
+  '/scripts',
+  express.static(`${__dirname}/node_modules/`)
+);
+
+app.listen(port, () => {
+  console.log('listening on %d', port);
+});
